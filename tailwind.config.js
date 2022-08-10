2110e3ef4a694857749a96e4c8ba5db65c8d6006
Sajjad Murtaza <sajjadmurtaza.nxb@gmail.com>
2022-08-10 22:31:50 +0200

    change colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#ec4755',
+        primary: '#af7f29',
         secondary: '#a12c34',
         tertiary: '#99a0a3',
         border: '#1a2e35',
